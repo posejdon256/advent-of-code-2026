4abe726e69ea93cbee2d98d0d06409c806da5718
posejdonik256 <anna@gyfted.me>
2025-12-02 19:31:49 +0100

    logging time

--- days/day-2/executor.ts
+++ days/day-2/executor.ts
@@ -1,15 +1,19 @@
+import { dataOne } from "./data.ts";
 import { one } from "./one.ts";
+import { splitData } from "./splitter.ts";
 import { two } from "./two.ts";
 import { units } from "./units.ts";
-import { dataOne, dataTwo, exampleData } from "./data.ts";
-import { splitData } from "./splitter.ts";
 
 const executor = (): void => {
     const areUnitsOk = units(splitData);
     if(areUnitsOk) {
         console.log('UNIT TESTS - OK')
+        console.time('one')
         console.log('ONE RESULT - ', one(dataOne));
+        console.timeEnd('one')
+        console.time('two')
         console.log('TWO RESULT - ', two(dataOne));
+        console.timeEnd('two')
         return;
     }
     console.log('UNIT TEST - FAILED')
--- days/template/executor.ts
+++ days/template/executor.ts
@@ -7,8 +7,12 @@ const executor = (): void => {
     const areUnitsOk = units();
     if(areUnitsOk) {
         console.log('UNIT TESTS - OK')
+        console.time('one');
         console.log('ONE RESULT - ', one(dataOne));
+        console.timeEnd('one');
+        console.time('two');
         console.log('TWO RESULT - ', two(dataOne));
+        console.timeEnd('two');
         return;
     }
     console.log('UNIT TEST - FAILED')
